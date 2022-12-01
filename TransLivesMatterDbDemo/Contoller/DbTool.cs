64c50e0df46bec67ce3b4c27b50f24dee0e099e0
k <k@K3-SSD>
2022-12-01 21:47:43 +0100

    App

---
+++ TransLivesMatterDbDemo/Contoller/DbTool.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Xml.Linq;
+using System.Data.Common;
+
+namespace TransLivesMatterDbDemo.Contoller
+{
+    class DbTool
+    {
+        private readonly string _dbServer;
+        private string _dbName;
+
+        private string _dbUser;
+
+        private string _dbPass;
+
+        public DbTool(string dbName, string dbUser, string dbPass, string dbServer = @".\SQLExpress")
+        {
+            _dbServer = dbServer;
+            _dbName = dbName;
+            _dbUser = dbUser;
+            _dbPass = dbPass;
+        }
+        public DbTool()
+        {
+
+            _dbName = "DevData";
+            _dbUser = "rainbow";
+            _dbPass = "everywhere";
+        }
+
+        public string TestConnection(out bool hasError, string dbName = "", string dbUser="", string dbPass = "")
+        {
+            hasError = false;
+            if (dbName != string.Empty)
+            {
+                _dbName = dbName;
+            }
+            if (dbUser != string.Empty)
+            {
+                _dbUser = dbUser;
+            }
+            if (dbPass != string.Empty)
+            {
+                _dbPass = dbPass;
+            }
+
+            string log = "";
+            string connectionString = @"Data Source=" + _dbServer + @";Initial Catalog=" + _dbName + @";User ID="+ _dbUser+@";Password="+ _dbPass + @"";
+            using (SqlConnection connection = new(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    log += "Connection open!" + Environment.NewLine;
+                }
+                catch (SqlException ex)
+                {
+                    log += (ex.ToString());
+                    hasError = true;
+                }
+                catch (Exception ex)
+                {
+                    log += (ex.ToString());
+                    hasError = true;
+                    throw;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+            return log;
+        }
+
+        public DataTable GetTableColumns(out bool hasError, ref string statusLog)
+        {
+            hasError = false;
+            DataTable dataTable = new DataTable();
+
+            string connectionString = @"Data Source=" + _dbServer + @";Initial Catalog=" + _dbName + @";User ID=" + _dbUser + @";Password=" + _dbPass + @"";
+            using (SqlConnection connection = new(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+                    SqlCommand command = connection.CreateCommand();
+                    command.CommandText = @"select TABLE_CATALOG, TABLE_NAME, COLUMN_NAME, DATA_TYPE
+from INFORMATION_SCHEMA.COLUMNS
+where TABLE_CATALOG = '"+_dbName+"' and DATA_TYPE = 'int'";
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        if (reader.HasRows)
+                        {
+                            dataTable = new();
+                            dataTable.Load(reader);
+                            statusLog += "Columns: " + dataTable.Columns.Count + Environment.NewLine + "Rows: " + dataTable.Rows.Count + Environment.NewLine;
+                        }
+                        else
+                        {
+                            statusLog += "Columns: " + 0 + Environment.NewLine + "Rows: " + 0 + Environment.NewLine;
+                        }
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    hasError = true;
+                    statusLog += ex.ToString();
+                    throw;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+            }
+
+                return dataTable;
+
+        }
+    }
+
+}
--- TransLivesMatterDbDemo/MainWindow.xaml.cs
+++ TransLivesMatterDbDemo/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -23,6 +25,39 @@ namespace TransLivesMatterDbDemo
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        TransLivesMatterDbDemo.Contoller.DbTool DbTool;
+
+        private void BtnTestConn_Click(object sender, RoutedEventArgs e)
+        {
+            DbTool = new(TxtDbName.Text, TxtDbLogin.Text, TxtDbPass.Password);
+
+            bool hasError;
+            string log = DbTool.TestConnection(out hasError);
+            TxtStatusLog.Text = log;
+            if (!hasError)
+            {
+                BtnLoadData.IsEnabled = true;
+            }
+        }
+
+        private void BtnLoadData_Click(object sender, RoutedEventArgs e)
+        {
+            bool hasError;
+            string log = TxtStatusLog.Text;
+            DataTable dataTable = DbTool.GetTableColumns(out hasError, ref log);
+            if (!hasError)
+            {
+                DtGrDataFromDb.AutoGenerateColumns = true;
+                DtGrDataFromDb.DataContext= dataTable.DefaultView;
+                TxtStatusLog.Text = log;
+                DtGrDataFromDb.CanUserAddRows = false;
+            }
+            else
+            {
+                TxtStatusLog.Text = log;
+            }
         }
     }
 }
